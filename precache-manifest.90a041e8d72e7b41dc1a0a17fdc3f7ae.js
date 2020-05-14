19f811202c30f0ecb723d6907dfacf69da37b9d3
shir0206 <40990488+shir0206@users.noreply.github.com>
2020-05-14 21:02:22 +0300

    Updates

---
+++ precache-manifest.90a041e8d72e7b41dc1a0a17fdc3f7ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffba8192827208703884b0a32fed6783",
+    "url": "/ArtGallery/index.html"
+  },
+  {
+    "revision": "14d0276caca4b8015d52",
+    "url": "/ArtGallery/static/css/main.aeb7708e.chunk.css"
+  },
+  {
+    "revision": "5b3573391e9ca79d55cb",
+    "url": "/ArtGallery/static/js/2.3f246fd4.chunk.js"
+  },
+  {
+    "revision": "8c49b40e63e9223e0666ac1bd39a1683",
+    "url": "/ArtGallery/static/js/2.3f246fd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14d0276caca4b8015d52",
+    "url": "/ArtGallery/static/js/main.01c1ae42.chunk.js"
+  },
+  {
+    "revision": "666e5c961e3dbfcc5a51",
+    "url": "/ArtGallery/static/js/runtime-main.3bde1e38.js"
+  }
+]);
