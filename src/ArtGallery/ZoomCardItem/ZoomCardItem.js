2203b3863962e8c8dd413590ad817f5ed6dfbc21
shir0206 <40990488+shir0206@users.noreply.github.com>
2020-05-16 23:41:35 +0300

    Refractor ZoomCardItem- add Like,Dislike,ZoomImage

--- src/ArtGallery/ZoomCardItem/ZoomCardItem.js
+++ src/ArtGallery/ZoomCardItem/ZoomCardItem.js
@@ -6,95 +6,11 @@ import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 export const ZoomCardItem = (props) => {
   const card = props.card;
 
-  //Like state
-  const [dislikeClassName, setDislikeClassName] = useState(false);
-  const [dislikeCounter, setDislikeCounter] = useState(function () {
-    return parseInt(props.card.likes, 10);
-  });
-
-  //Dislike state
-  const [likeClassName, setLikeClassName] = useState(false);
-  const [likeCounter, setLikeCounter] = useState(function () {
-    return parseInt(props.card.likes, 10);
-  });
-
-  //Like
-  useEffect(() => {
-    setLikeCounter(parseInt(props.card.likes, 10));
-  }, [props.card.likes]); //[]
-
-  useEffect(() => {
-    setLikeCounter(parseInt(props.card.likes, 10));
-  }, [props]); //[props]
-
-  //Dislike
-  useEffect(() => {
-    setDislikeCounter(parseInt(props.card.dislikes, 10));
-  }, [props.card.dislikes]); //[]
-
-  useEffect(() => {
-    setDislikeCounter(parseInt(props.card.dislikes, 10));
-  }, [props]);
-
-  //Like
-  function handleLike() {
-    updateLikeDB();
-    //cookie
-  }
-
-  //Dislike
-  function handleDislike() {
-    updateDislikeDB();
-
-    //cookie
-  }
-
-  //Like
-  function updateLikeDB() {
-    var updates = {};
-    updates["/Cards/" + card.id + "/likes"] = likeCounter + 1;
-    firebase
-      .database()
-      .ref()
-      .update(updates, function (error) {
-        if (error) {
-          // alert("add like failed");
-          return false;
-        } else {
-          // Data saved successfully, update the view of the likes counter
-          setLikeClassName(!likeClassName);
-          setLikeCounter(likeCounter + 1);
-          return true;
-        }
-      });
-  }
-
-  //Dislike
-  function updateDislikeDB() {
-    var updates = {};
-    updates["/Cards/" + card.id + "/dislikes"] = dislikeCounter + 1;
-    firebase
-      .database()
-      .ref()
-      .update(updates, function (error) {
-        if (error) {
-          // alert("add like failed");
-          return false;
-        } else {
-          // Data saved successfully, update the view of the likes counter
-          setDislikeClassName(!dislikeClassName);
-          setDislikeCounter(dislikeCounter + 1);
-          return true;
-        }
-      });
-  }
-
   const [search, setSearch] = useState("");
 
   // Recieve search data from Search component, init search state
   const recieveTagText = useCallback(
     (childProps) => {
-      console.log("inside recieveTagText", childProps);
       // Update searched text in the state
       setSearch(childProps);
 
@@ -110,35 +26,7 @@ export const ZoomCardItem = (props) => {
   return (
     <div className="zoom-wrapper">
       <div className="zoom-container">
-        <div id="imageContainer" className="image-container">
-          <TransformWrapper>
-            {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
-              <React.Fragment>
-                <div className="image-wrapper">
-                  <TransformComponent>
-                    <img
-                      id="image"
-                      className="image"
-                      src={card.imgURL}
-                      alt={card.title}
-                    />
-                  </TransformComponent>
-                </div>
-                <div className="tools">
-                  <button className="zoom-in-btn" onClick={zoomIn}>
-                    <i className="fas fa-search-plus zoom-in-icon" />
-                  </button>
-                  <button className="zoom-out-btn" onClick={zoomOut}>
-                    <i className="fas fa-search-minus zoom-out-icon" />
-                  </button>
-                  <button className="zoom-reset-btn" onClick={resetTransform}>
-                    <i className="fas fa-expand zoom-reset-icon" />
-                  </button>
-                </div>
-              </React.Fragment>
-            )}
-          </TransformWrapper>
-        </div>
+        <ZoomImage card={card} />
         <div id="detailsContainer" className="details-container">
           <p id="title" className="title">
             {card.title}
@@ -150,33 +38,8 @@ export const ZoomCardItem = (props) => {
           </p>
           <ul>{tagsList}</ul>
           <div className="likes-container">
-            <div className="like-container">
-              <i
-                className={
-                  likeClassName
-                    ? "fa fa-heart like-icon like-icon-anim"
-                    : "fa fa-heart like-icon "
-                }
-                onClick={handleLike}
-                onAnimationEnd={() => setLikeClassName(!likeClassName)}
-              />
-              <p id="likes">{likeCounter}</p>
-            </div>
-            <div className="dislike-container">
-              <i
-                className={
-                  dislikeClassName
-                    ? "fas fa-heart-broken dislike-icon dislike-icon-anim"
-                    : "fas fa-heart-broken dislike-icon"
-                }
-                onClick={handleDislike}
-                onAnimationEnd={() => setDislikeClassName(!dislikeClassName)}
-
-              />
-              <p id="dislikes" className="dislikes">
-                {dislikeCounter}
-              </p>
-            </div>
+            <Like card={card} />
+            <Dislike card={card} />
           </div>
         </div>
       </div>
@@ -197,7 +60,6 @@ function Tag(props) {
 
   function updateSearch(event) {
     event.preventDefault();
-    console.log("inside updateSearch", tag);
 
     // Get the search text when occurs event 'onChange'
     setSearch(tag);
@@ -210,5 +72,150 @@ function Tag(props) {
     <li className="tag" onClick={updateSearch}>
       #{tag}
     </li>
+  );
+}
+
+function ZoomImage(props) {
+  let card = props.card;
+  return (
+    <div id="imageContainer" className="image-container">
+      <TransformWrapper>
+        {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+          <React.Fragment>
+            <div className="image-wrapper">
+              <TransformComponent>
+                <img
+                  id="image"
+                  className="image"
+                  src={card.imgURL}
+                  alt={card.title}
+                />
+              </TransformComponent>
+            </div>
+            <div className="tools">
+              <button className="zoom-in-btn" onClick={zoomIn}>
+                <i className="fas fa-search-plus zoom-in-icon" />
+              </button>
+              <button className="zoom-out-btn" onClick={zoomOut}>
+                <i className="fas fa-search-minus zoom-out-icon" />
+              </button>
+              <button className="zoom-reset-btn" onClick={resetTransform}>
+                <i className="fas fa-expand zoom-reset-icon" />
+              </button>
+            </div>
+          </React.Fragment>
+        )}
+      </TransformWrapper>
+    </div>
+  );
+}
+
+function Like(props) {
+  let card = props.card;
+
+  const [likeClassName, setLikeClassName] = useState(false);
+  const [likeCounter, setLikeCounter] = useState(function () {
+    return parseInt(props.card.likes, 10);
+  });
+
+  useEffect(() => {
+    setLikeCounter(parseInt(props.card.likes, 10));
+  }, [props.card.likes]);
+
+  useEffect(() => {
+    setLikeCounter(parseInt(props.card.likes, 10));
+  }, [props]);
+
+  function handleLike() {
+    updateLikeDB();
+  }
+
+  function updateLikeDB() {
+    var updates = {};
+    updates["/Cards/" + card.id + "/likes"] = likeCounter + 1;
+    firebase
+      .database()
+      .ref()
+      .update(updates, function (error) {
+        if (error) {
+          return false;
+        } else {
+          // Data saved successfully, update the view of the likes counter
+          setLikeClassName(!likeClassName);
+          setLikeCounter(likeCounter + 1);
+          return true;
+        }
+      });
+  }
+
+  return (
+    <div className="like-container">
+      <i
+        className={
+          likeClassName
+            ? "fa fa-heart like-icon like-icon-anim"
+            : "fa fa-heart like-icon "
+        }
+        onClick={handleLike}
+        onAnimationEnd={() => setLikeClassName(!likeClassName)}
+      />
+      <p id="likes">{likeCounter}</p>
+    </div>
+  );
+}
+
+function Dislike(props) {
+  let card = props.card;
+
+  const [dislikeClassName, setDislikeClassName] = useState(false);
+  const [dislikeCounter, setDislikeCounter] = useState(function () {
+    return parseInt(props.card.likes, 10);
+  });
+
+  useEffect(() => {
+    setDislikeCounter(parseInt(props.card.dislikes, 10));
+  }, [props.card.dislikes]);
+
+  useEffect(() => {
+    setDislikeCounter(parseInt(props.card.dislikes, 10));
+  }, [props]);
+
+  function handleDislike() {
+    updateDislikeDB();
+  }
+
+  function updateDislikeDB() {
+    var updates = {};
+    updates["/Cards/" + card.id + "/dislikes"] = dislikeCounter + 1;
+    firebase
+      .database()
+      .ref()
+      .update(updates, function (error) {
+        if (error) {
+          return false;
+        } else {
+          // Data saved successfully, update the view of the likes counter
+          setDislikeClassName(!dislikeClassName);
+          setDislikeCounter(dislikeCounter + 1);
+          return true;
+        }
+      });
+  }
+
+  return (
+    <div className="dislike-container">
+      <i
+        className={
+          dislikeClassName
+            ? "fas fa-heart-broken dislike-icon dislike-icon-anim"
+            : "fas fa-heart-broken dislike-icon"
+        }
+        onClick={handleDislike}
+        onAnimationEnd={() => setDislikeClassName(!dislikeClassName)}
+      />
+      <p id="dislikes" className="dislikes">
+        {dislikeCounter}
+      </p>
+    </div>
   );
 }
